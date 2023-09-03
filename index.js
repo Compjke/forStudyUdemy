0d6b17394b5497175325447ac7c57bd48ec52f3b
Alex Suvorov <compjkee@gmail.com>
2023-09-03 21:26:49 +0300

    create shopopalo

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ const divContainer = document.querySelector('.container')
 divContainer.textContent = 'Hello from JS'
 
 
+const numberOfFilms = prompt(
+  "How many films were you watched",
+  "Specify the number"
+);
 
 
+const personalMovieDB = {
+   count : numberOfFilms,
+   movies : {},
+   actors : {},
+   genres : [],
+   privat : false
+}
+
+
+
+const lastFilm = prompt('Last film?', '')
+const filmScore = +prompt('Your score', '')
+
+personalMovieDB.movies[lastFilm] = filmScore
+
+console.log(personalMovieDB)
